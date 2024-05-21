3497c62c1f55113b11c3ee3874dc517112ebe404
fcscostasantos <fcscostasantos@gmail.com>
2024-05-21 14:05:10 -0300

    Adiciona arquivo static

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gestao_escolar',
         'USER': 'postgres',
-        'PASSWORD': 'GestEscolar@2024!',
+        'PASSWORD': 'S@s27fcs!',
         'HOST': 'localhost',
         'PORT': '5432',
     }
